d5c653decf4cfc0d8053989ff84b99788aaadf55
fdaubercy <fdaubercy@gmail.com>
2026-07-15 20:04:31 +0200

    chore: sorties de build (data_dir et defines Berry)
    Aucune modification manuelle : ces trois fichiers sont reecrits a chaque
    build par les post-scripts de PlatformIO.
    
    - platformio_override.ini : data_dir suit l'environnement compile en
      dernier (adapteParametresPlatformio_override) -> data/etage2/tasmota32s3-grenier.
    - tasmota_defines_for_berry.h : ~3200 defines extraits par le preprocesseur
      (dump-defines.py), pour que le solidifieur voie les memes USE_* que le
      compilateur C++. Le script amont le dit gitignore, il est pourtant suivi
      ici : son diff reflete la reactivation de USE_BERRY_DEBUG.
    - tasmota_defines_for_berry.be : son pendant cote Berry.

--- tasmota/tasmota_defines_for_berry.h
+++ tasmota/tasmota_defines_for_berry.h
@@ -1,5 +1,6 @@
 #define MQTT_USE true
 #define D_SENSOR_DHT11 "DHT11"
+#define D_MODBUSRELAY_RX_ENA "MbsRelay RX Ena"
 #define D_PROGRAM_VERSION "Version du programme"
 #define D_SENT_TO "envoyé à"
 #define COLOR_TITLE_TEXT "#eaeaea"
@@ -169,8 +170,8 @@
 #define D_SENSOR_SDIO_D2 "SDIO D2"
 #define D_SENSOR_SDIO_D3 "SDIO D3"
 #define COLOR_TIMER_TAB_TEXT "#faffff"
-#define STA_SSID1 "SERVEUR-GARAGE-GATEWAY"
-#define STA_SSID2 "iPhone de Frederic"
+#define STA_SSID1 "MAISON"
+#define STA_SSID2 "Relai Wifi 2.4G KuWFi"
 #define D_SUBNET_MASK "Masque sous-réseau"
 #define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1 1
 #define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2 1
@@ -219,7 +220,7 @@
 #define D_CMND_PIP_QMOD "QMOD"
 #define D_SENSOR_BOILER_OT_TX "OpenTherm TX"
 #define D_SCRIPT_CHARS_LEFT "car. restant"
-#define WIFI_GATEWAY "192.168.4.1"
+#define WIFI_GATEWAY "192.168.0.254"
 #define D_SENSOR_LD2410_RX "LD2410 Rx"
 #define D_JSON_LINK_COUNT "LinkCount"
 #define __GCC_ATOMIC_CHAR32_T_LOCK_FREE 2
@@ -419,7 +420,7 @@
 #define D_ENTER_COMMAND "Saisir une commande"
 #define D_FLOWRATEMETER_NAME "Débit"
 #define USE_MGS 
-#define MQTT_GRPTOPIC "tasmotas/garage"
+#define MQTT_GRPTOPIC "tasmotas/etage2"
 #define USE_SUNRISE 
 #define D_SENSOR_MIEL_HVAC_TX "MiEl HVAC Tx"
 #define WIFI_SENSITIVITY_54g -750
@@ -465,7 +466,7 @@
 #define D_SENSOR_CC1101_CS "CC1101 CS"
 #define D_CMND_LEDPWM_OFF "LedPwmOff"
 #define D_JSON_IRHVAC_CELSIUS "Celsius"
-#define PROJECT "RIDEAU-GARAGE"
+#define PROJECT "SERVEUR-GRENIER-2EME"
 #define D_UTC_TIME "UTC"
 #define __UINT32_MAX__ 0xffffffffUL
 #define D_CHARGE "Charge"
@@ -496,7 +497,7 @@
 #define D_FAILED "Échoué"
 #define D_SENSOR_ZIGBEE_RST "ZigBee Rst"
 #define D_UNIT_KILOMETER_PER_HOUR "km/h"
-#define WIFI_CONFIG_TOOL WIFI_WAIT
+#define WIFI_CONFIG_TOOL WIFI_MANAGER
 #define D_JSON_SERIALRECEIVED "SerialReceived"
 #define D_CMND_PREFIX "Prefix"
 #define D_THERMOSTAT_RAMP_UP "Ramp up"
@@ -585,7 +586,6 @@
 #define D_SENSOR_SAIR_RX "SAir Rx"
 #define D_SENSOR_CC1101_GDO2 "CC1101 GDO2"
 #define D_FRAME_RATE "Fréq. image"
-#define USE_WEBFILES 
 #define USE_ZIGBEE_AUTOBIND_PRESSURE 1
 #define D_JSON_CONNECT_FAILED "Connect failed"
 #define D_LOG_DOMOTICZ "DOM: "
@@ -671,7 +671,6 @@
 #define USE_LVGL_PSRAM 
 #define D_RESTORE_CONFIGURATION "Restauration de la config."
 #define D_JSON_HUMIDITY "Humidity"
-#define D_SYSLOG_HOST "Hôte SysLog"
 #define IR_RCV_TOLERANCE 25
 #define D_CONFIRM_RESTART "Confirmer le redémarrage"
 #define D_MQTT "MQTT"
@@ -797,7 +796,6 @@
 #define D_SENSOR_C8_CO2_5K_TX "C8-CO2-5K Tx"
 #define APP_LEDSTATE LED_POWER
 #define D_PROGRAM_FLASH_SIZE "Taille Flash Programme"
-#define USE_MODBUS 
 #define memcpy_P memcpy
 #define D_CMND_PUBLISH "Publish"
 #define D_CMND_SHUTTER_CLOSETIME "CloseDuration"
@@ -826,7 +824,6 @@
 #define D_SENSOR_NEOPOOL_TX "NeoPool Tx"
 #define __XCHAL_HAVE_DIV32 1
 #define GUI_EDIT_FILE 
-#define USE_HTTPSERVER 
 #define D_JSON_MOTOR_RPM "setRPM"
 #define D_CMND_SEQUENCE_OFFSET "SequenceOffset"
 #define USE_SGP40 
@@ -843,7 +840,7 @@
 #define FM24CXX_JSON_MAX_BYTES 4096
 #define D_CMND_UPLOAD "Upload"
 #define D_JSON_DOWNTIME "Downtime"
-#define FRIENDLY_NAME "Rideau de Garage"
+#define FRIENDLY_NAME "Serveur Grenier 2eme"
 #define D_UNIT_CELSIUS "C"
 #define D_JSON_IR_DATA "Data"
 #define D_DOMOTICZ_IDX "Idx"
@@ -862,7 +859,7 @@
 #define D_CMND_WAKEUPDURATION "WakeUpDuration"
 #define D_MQTT_SERVICE_FOUND "Service MQTT trouvé sur"
 #define __FLT64_EPSILON__ 2.2204460492503131e-16F64
-#define USER_TEMPLATE "{\"NAME\":\"ESP32S3 Rideau Garage Modbus\",\"GPIO\":[1,1,1,1,3232,3200,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,544,288,1,224,225,1,1,1,1,1,1s],\"FLAG\":0,\"BASE\":1}"
+#define USER_TEMPLATE "{\"NAME\":\"ESP32S3 Trappe\",\"GPIO\":[1,1,1,1,32,1,1,1,1,1,1,1,1,288,1,1,1,1,1,1,1,1,0,0,0,0,0,544,1,224,225,226,1,1,1,1,1,1],\"FLAG\":0,\"BASE\":1}"
 #define APP_INTERLOCK_MODE false
 #define USE_FTP 
 #define __INT16_MAX__ 0x7fff
@@ -887,6 +884,8 @@
 #define D_SENSOR_TUYAMCUBR_RX "TuyaMCUBr Rx"
 #define __LDBL_HAS_QUIET_NAN__ 1
 #define D_UNIT_KILOWATTHOUR "kWh"
+#define AP_BSSID1 ""
+#define AP_BSSID2 ""
 #define __cpp_return_type_deduction 201304L
 #define __XCHAL_HAVE_BOOLEANS 1
 #define D_SO_ZIGBEE_NOAUTOQUERY "NoAutoQuery"
@@ -925,6 +924,7 @@
 #define USE_ADE7880 
 #define D_MANAGE_FILE_SYSTEM "Gestion du Système de Fichier"
 #define LIGHT_ALEXA_CT_RANGE false
+#define USE_SHA_ROM 1
 #define __GNUC__ 15
 #define D_SENSOR_INTERRUPT "Interrupt"
 #define D_STATUS7_TIME "TIM"
@@ -958,7 +958,7 @@
 #define D_CMND_UPGRADE "Upgrade"
 #define D_CMND_ZIGBEE_FORGET "Forget"
 #define USE_ZIGBEE_AUTOBIND_LIFT 1.0
-#define MQTT_TOPIC "garage/rideau"
+#define MQTT_TOPIC "etage2/grenier"
 #define SUPPORT_IF_STATEMENT 
 #define D_FP_UPLOADFEATUREFAIL "Erreur de transfert"
 #define WEB_SERVER 2
@@ -1112,7 +1112,6 @@
 #define D_CONFIGURATION_SAVED "Configuration enregistrée"
 #define __cpp_ref_qualifiers 200710L
 #define WS2812_LEDS 30
-#define TASMOTAMODBUSDEBUG 
 #define __INT_LEAST32_MAX__ 0x7fffffffL
 #define D_CMND_PIP_QPIRI "QPIRI"
 #define D_CONSOLE "Console"
@@ -1400,11 +1399,12 @@
 #define D_CMND_SWITCHTEXT "SwitchText"
 #define D_MULTI_DEVICE "multi module"
 #define __XSHAL_HAVE_TEXT_SECTION_LITERALS 1
+#define D_JSON_POWERUSAGE_TOTAL "PowerTotal"
 #define D_CONFIGURE_OTHER "Autre configuration"
 #define D_SENSOR_BL0940_RX "BL0940 Rx"
 #define D_JSON_SWITCH "Switch"
 #define D_CMND_RFSYNC "Sync"
-#define USER_BACKLOG "Backlog Hostname RIDEAU-GARAGE"
+#define USER_BACKLOG "Backlog Module 0; Hostname SERVEUR-GRENIER-2EME"
 #define D_LOG_MQTT "MQT: "
 #define D_CMND_SAFEPOWER "SafePower"
 #define D_FP_IMAGEFAIL "Erreur d'acquisition"
@@ -1549,6 +1549,7 @@
 #define D_DOMOTICZ_ILLUMINANCE "Éclairement"
 #define D_JSON_PREAMBLE_LENGTH "PreambleLength"
 #define D_TEMPLATE_WRONG_ARCH "Reset to correct chip type"
+#define D_JSON_EXPORT_ACTIVE_TOTAL "ExportActiveTotal"
 #define D_HUE_API "Hue API"
 #define D_SENSOR_GPS_RX "GPS Rx"
 #define DS18X20_PULL_UP false
@@ -1565,7 +1566,7 @@
 #define D_SENSOR_BIOPDU_PZEM0XX_TX "BioPDU PZEM0XX Tx"
 #define USE_SDM120 
 #define FM24CXX_BLOCK_SIZE 256
-#define CFG_HOLDER 1326
+#define CFG_HOLDER 1337
 #define D_MODULE "Module"
 #define D_SENSOR_WS2812 "WS2812"
 #define D_SO_MQTTTLS "MqttTLS"
@@ -1852,7 +1853,7 @@
 #define D_CMND_MQTTWIFITIMEOUT "MqttWifiTimeout"
 #define D_FREQUENCY "Fréquence"
 #define D_TOTAL_ACTIVE "Total Active"
-#define WIFI_RGX_SSID "RIDEAU-GARAGE-GATEWAY"
+#define WIFI_RGX_SSID "SERVEUR-GRENIER-2EME"
 #define MAX31865_PTD_WIRES 2
 #define __FLT32X_MIN__ 2.2250738585072014e-308F32x
 #define __XCHAL_HAVE_DFP_SQRT 0
@@ -1913,6 +1914,7 @@
 #define D_CMND_OTAURL "OtaUrl"
 #define D_JSON_ZIGBEE_PING "ZbPing"
 #define D_JSON_ZIGBEE_BIND "ZbBind"
+#define FIRMWARE_ESP32S3_ETAGE2_GRENIER 1
 #define D_SENSOR_SR04_TRIG "SR04 Tri/Tx"
 #define D_EMULATION "Émulation"
 #define D_HRG_EVENT "Event"
@@ -1927,6 +1929,7 @@
 #define D_WITH_IP_ADDRESS "avec l'adresse IP"
 #define D_JSON_CURRENT "Current"
 #define D_SERIAL_LOGGING_DISABLED "Journalisation série désactivée"
+#define D_MODBUSRELAY_TX_ENA "MbsRelay TX Ena"
 #define SEND_NEC true
 #define KNX_DIMMER 32
 #define D_BUTTON_TOGGLE "on/off"
@@ -1961,7 +1964,7 @@
 #define D_HOSTED_MCU "Hosted MCU"
 #define D_SO_ALEXACTRANGE "AlexaCTRange"
 #define D_JSON_IRHVAC_VENDOR "Vendor"
-#define STA_PASS2 "Lune5676"
+#define STA_PASS2 "obdormisti-pervigile%.-ficiendus"
 #define BE_LV_WIDGET_SPINNER 
 #define D_CMND_HDMI_SEND "Send"
 #define USE_PN532_HSU 
@@ -2173,12 +2176,13 @@
 #define D_TO "à"
 #define D_AS3935_CAL_OK "calibration établie à :"
 #define D_SENSOR_LE01MR_RX "LE-01MR Rx"
-#define WIFI_IP_ADDRESS "0.0.0.0"
+#define WIFI_IP_ADDRESS "192.168.0.47"
 #define D_SENSOR_DS18X20 "DS18x20"
 #define __FLT32_MIN_10_EXP__ (-37)
 #define D_FP_PASSVERIFY "Mot-de-passe vérifié"
 #define D_TIMER_DAYS "Jours"
 #define D_UNIT_FPS "FPS"
+#define D_MODBUSRELAY_RX "MbsRelay RX"
 #define D_JSON_VOLUME "Volume"
 #define D_JSON_WRONG "Wrong"
 #define D_ENABLE_WEBLOG_FOR_RESPONSE "Activer WebLog 2 si une réponse est attendue"
@@ -2222,6 +2226,7 @@
 #define APP_BLINKCOUNT 10
 #define D_JSON_POWERFACTOR "Factor"
 #define __cpp_range_based_for 201603L
+#define D_MODBUSRELAY_TX "MbsRelay TX"
 #define D_CMND_ZIGBEE_REPORT "Report"
 #define D_SO_MQTTJSONONLY "MqttJSONOnly"
 #define TELE_PERIOD 300
@@ -2264,10 +2269,9 @@
 #define D_PARTITION "Partition"
 #define BE_LV_WIDGET_OBJ 
 #define D_UNIT_MILIGRAMS_PER_LITER "mg/L"
-#define USE_LORA_SX126X_DEBUG 
 #define HUMIDITY_RESOLUTION 1
 #define D_CMND_TIME "Time"
-#define STA_PASS1 "Lune5676"
+#define STA_PASS1 "obdormisti-pervigile%.-ficiendus"
 #define D_MINUTE_SECOND_SEPARATOR ":"
 #define D_CMND_MQTTLOG "MqttLog"
 #define __INTPTR_TYPE__ int
@@ -2276,7 +2280,6 @@
 #define __WCHAR_TYPE__ short unsigned int
 #define __XCHAL_HAVE_DEPBITS 0
 #define D_GPIO_SHIFT595_SER "74x595 Ser"
-#define USE_MODBUS_BRIDGE_TCP 
 #define __SIZEOF_FLOAT__ 4
 #define USE_DEVICE_GROUPS 
 #define ESP32 1
@@ -2378,7 +2381,7 @@
 #define D_FREE_MEMORY "Mémoire libre"
 #define D_DISTANCE "Distance"
 #define D_COLOR_RED "Rouge"
-#define MQTT_CLIENT_ID "RIDEAU-GARAGE"
+#define MQTT_CLIENT_ID "SERVEUR-GRENIER-2EME"
 #define D_SENSOR_GM861_TX "GM861 Tx"
 #define D_VERSION "Version"
 #define THERMOSTAT_TIME_MAX_OUTPUT_INCONSIST 3
@@ -2430,7 +2433,7 @@
 #define THERMOSTAT_TIME_MIN_ACTION 4
 #define D_SENSOR_LED "LED"
 #define __cpp_inheriting_constructors 201511L
-#define CODE_IMAGE_STR "Rideau de garage"
+#define CODE_IMAGE_STR "Serveur Grenier 2eme"
 #define D_RSLT_INFO "INFO"
 #define MQTT_RESULT_COMMAND false
 #define __INT_LEAST64_MAX__ 0x7fffffffffffffffLL
@@ -2531,6 +2534,7 @@
 #define USE_ARILUX_RF 
 #define __XCHAL_HAVE_SEXT 1
 #define USE_SPI 
+#define USE_BERRY_DEBUG 
 #define __INT_LEAST32_TYPE__ long int
 #define D_CMND_MAXENERGYSTART "MaxEnergyStart"
 #define D_JSON_STACKLOWMARK "StackLowMark"
@@ -2584,9 +2588,9 @@
 #define D_FLOWRATEMETER_AMOUNT_TODAY "Quantité aujourd'hui"
 #define D_SENSOR_MCP39F5_TX "MCP39F5 Tx"
 #define DAWN_ASTRONOMIC -18.0
+#define D_SYSLOG_HOST "Hôte SysLog"
 #define D_SHT1X_FOUND "SHT1X trouvé"
 #define USE_ZIGBEE_AUTOBIND_TEMPERATURE 0.5
-#define FIRMWARE_ESP32S3_RIDEAU_GARAGE_MODBUS 1
 #define D_JSON_MQTT_TLS "MqttTLS"
 #define DNS_TIMEOUT 1000
 #define D_SIGNALSTRENGTH "Signal Strength"
@@ -2834,7 +2838,7 @@
 #define __XCHAL_HAVE_FP_RSQRT 1
 #define D_INFO "Info"
 #define USE_APDS9960_PROXIMITY 
-#define USE_WS2812_RMT 0
+#define USE_WS2812_RMT 1
 #define D_WEBSERVER_ACTIVE_ON "Serveur web actif sur"
 #define D_SENSOR_SAIR_TX "SAir Tx"
 #define D_NOW_YOU_CAN_CLOSE_THIS_WINDOW "Vous pouvez fermer cette fenêtre"
@@ -2844,7 +2848,6 @@
 #define D_CMND_STATERETAIN "StateRetain"
 #define D_DOMOTICZ_TEMP_HUM_BARO "Temp,Hum,Baro"
 #define USE_WS2812_CTYPE NEO_GRB
-#define USE_WSSERVER 
 #define D_CMND_HDMI_ADDR "Addr"
 #define D_TARIFF "Tarif"
 #define __INT32_TYPE__ long int
@@ -3065,6 +3068,7 @@
 #define D_KNX_WARNING "L'Adresse de Groupe (0/0/0) est réservée et ne peut être utilisée."
 #define D_SENSOR_TXD "Série Tx"
 #define __GCC_ATOMIC_LLONG_LOCK_FREE 1
+#define D_CMND_BSSID "BSSId"
 #define USE_CONFIG_OVERRIDE 1
 #define D_AS3935_CAL_FAIL "défaut de calibration"
 #define D_SYSLOG_HOST_NOT_FOUND "Hôte SysLog introuvable"
